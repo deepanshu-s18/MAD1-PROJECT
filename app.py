a4af8218eb6f2f2c0f2f750d2fa03eecdfe5119f
23f3000048 <23f3000048@ds.study.iitm.ac.in>
2025-07-01 16:38:03 +0530

    Second Commit

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
-print("Hello World!")
+from flask import Flask
+
+app = Flask(__name__, template_folder = 'templates')
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
-    pass
+    app.run(debug=True, port = 8080)
+
+
